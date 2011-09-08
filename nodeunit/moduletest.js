fc3e5777ec0754134387341266c7fb669af5914f
Dave Geddes <davidcgeddes@gmail.com>
2011-09-07 23:52:14 -0600

    trying nodeunit again

--- nodeunit/moduletest.js
+++ nodeunit/moduletest.js
@@ -1,8 +1,21 @@
-define(['SampleModule'], function(sample) {
+(function (name, definition) {
+	var theModule = definition(),
+		hasDefine = typeof define === 'function',
+		hasExports = typeof module !== 'undefined' && module.exports;
+
+	if (hasDefine) { // AMD Module
+		define(theModule);
+	} else if (hasExports) { // Node.js Module
+		module.exports = theModule;
+	}
+})('moduletest', function () {
+
+	// define your module here and return the public API
 	return {
 		'example test': function (test) {
 			test.ok(true, 'everything is ok');
 			test.done();
 		}
 	};
+
 });
--- nodeunit/runner.js
+++ nodeunit/runner.js
@@ -1,26 +1,14 @@
-//todo will this work in the browser still?
 var requirejs = require('requirejs');
 
-require.paths.unshift(__dirname + '/deps');
-try {
-	var reporter = require('nodeunit').reporters.default;
-}
-catch(e) {
-	console.log("Cannot find nodeunit module.");
-	console.log("You can download submodules for this project by doing:");
-	console.log("");
-	console.log("    git submodule init");
-	console.log("    git submodule update");
-	console.log("");
-	process.exit();
-}
-
-process.chdir(__dirname);
-
-requirejs(['moduletest'], function(moduletest){
-	console.log('moduletest: ', moduletest);
-	reporter.run({
-		'Sample Module' : moduletest
-	});
+requirejs(['moduletest'], function(SampleModule){
+	console.log("SampleModule: ", SampleModule);
+//	exports.testSomething = function(test) {
+//		test.expect(1);
+//		test.ok(true, "this assertion should pass");
+//		test.done();
+//	};
 });
-//reporter.run(['scripttest.js']);
+
+
+
+
