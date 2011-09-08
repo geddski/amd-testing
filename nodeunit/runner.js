28c7046b598a9d1d4b329d08ad5af6ef3e3da9b9
Dave Geddes <davidcgeddes@gmail.com>
2011-09-08 00:41:18 -0600

    improved how modules are registered with the runner

--- nodeunit/runner.js
+++ nodeunit/runner.js
@@ -1,15 +1,11 @@
 var requirejs = require('requirejs');
 requirejs.config({
-	//Pass the top-level main.js/index.js require
-	//function to requirejs so that node modules
-	//are loaded relative to the top-level JS file.
 	nodeRequire: require,
 	baseUrl: 'js'
 });
 
 requirejs(['moduletest'], function(ModuleTest){
-	exports.testSomething = ModuleTest.testSomething;
-	exports.testSomethingElse = ModuleTest.testSomethingElse;
+	exports.ModuleTest = ModuleTest;
 });
 
 
