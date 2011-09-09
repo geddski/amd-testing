452f961eee019decc7cf975de94f87cec7d68223
Dave Geddes <davidcgeddes@gmail.com>
2011-09-09 16:45:56 -0600

    runner is working

--- jasmine/js/runner.js
+++ jasmine/js/runner.js
@@ -9,7 +9,7 @@ requirejs(['./spec/ModuleSpec'], function(ModuleSpec) {
 	var jasmine = require('./lib/jasmine-1.1.0.rc1/jasmine').jasmine;
 	var window = "hi";
 	var ConsoleJasmineReporter2 = require('./lib/consoleJasmineReporter2').ConsoleJasmineReporter;
-	//	console.log("ModuleSpec: ", ModuleSpec);
+//		console.log("ModuleSpec: ", ModuleSpec);
 //		exports.ModuleSpec = ModuleSpec;
 	jasmine.getEnv().addReporter(new ConsoleJasmineReporter2());
 	jasmine.getEnv().execute();
--- jasmine/js/spec/ModuleSpec.js
+++ jasmine/js/spec/ModuleSpec.js
@@ -1,29 +1,51 @@
+var jasmine = jasmine || require('../lib/jasmine-1.1.0.rc1/jasmine').jasmine;
+var describe = describe || require('../lib/jasmine-1.1.0.rc1/jasmine').describe;
+var it = it || require('../lib/jasmine-1.1.0.rc1/jasmine').it;
+var expect = expect || require('../lib/jasmine-1.1.0.rc1/jasmine').expect;
+var define = define || require('requirejs');
+
+define([], function(){
+	console.log('modulespec');
+	describe('module test', function() {
+		it('should work in the browser and node', function() {
+			expect(1).toEqual(1);
+		});
+
+		it('should work in node', function(){
+			expect(2).toEqual(2);
+		});
+});
+  return {
+	  name: "modulespec"
+  }
+});
+
 //todo find better way to make these globally available
 //var jasmine = jasmine || require('../lib/jasmine-1.1.0.rc1/jasmine').jasmine;
 //var describe = describe || require('../lib/jasmine-1.1.0.rc1/jasmine').describe;
 //var it = it || require('../lib/jasmine-1.1.0.rc1/jasmine').it;
 //var expect = expect || require('../lib/jasmine-1.1.0.rc1/jasmine').expect;
 
-console.log('modulespec');
 
-describe('module test', function() {
-//	var isNode = typeof module !== 'undefined' && module.exports;
-//	if(isNode){
-//	 	require = require('requirejs');
-//	}
 
-//	require(['SampleModule'], function(SampleModule){
-//		console.log("SampleModule: ", SampleModule);
-		it('should work', function() {
-			expect(3).toEqual(3);
-		});
-
-		it('should be awesome', function() {
-			expect(2).toEqual(2);
-		});
-
-		it('should work in the browser and node', function() {
-			expect(1).toEqual(1);
-		});
-//	});
-});
+//describe('module test', function() {
+////	var isNode = typeof module !== 'undefined' && module.exports;
+////	if(isNode){
+////	 	require = require('requirejs');
+////	}
+//
+////	require(['SampleModule'], function(SampleModule){
+////		console.log("SampleModule: ", SampleModule);
+//		it('should work', function() {
+//			expect(3).toEqual(3);
+//		});
+//
+//		it('should be awesome', function() {
+//			expect(2).toEqual(2);
+//		});
+//
+//		it('should work in the browser and node', function() {
+//			expect(1).toEqual(1);
+//		});
+////	});
+//});
