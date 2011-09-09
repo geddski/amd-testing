6e5649772f7a7c43542aaeafa8f9e6ccef5ef036
Dave Geddes <davidcgeddes@gmail.com>
2011-09-09 01:38:15 -0600

    didn't work

--- jasmine/js/runner2.js
+++
@@ -1,7 +0,0 @@
-var isVerbose = true;
-var showColors = true;
-
-var jasmine = require('jasmine-node');
-jasmine.executeSpecsInFolder(__dirname + '/spec', function(runner, log){
-	process.exit(runner.results().failedCount?1:0);
-}, isVerbose, showColors);
