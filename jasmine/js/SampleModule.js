626d6994b94d38fd396dc5411ea71b145505680b
David Mosher <davidmosher@gmail.com>
2011-10-21 09:48:57 -0600

    added jquery dependency for further clarification

--- jasmine/js/SampleModule.js
+++ jasmine/js/SampleModule.js
@@ -1,4 +1,8 @@
-define({
-  name: "sample",
-  purpose: "AMD testing"
+define(["jquery"], function($){
+  
+  return {
+    name: "sample",
+    purpose: "AMD testing",
+    jq_version: $().jquery
+  };
 });
--- jasmine/js/spec/ModuleSpec.js
+++ jasmine/js/spec/ModuleSpec.js
@@ -8,6 +8,10 @@ define(["../SampleModule"], function(SampleModule) {
     it('should state the purpose', function() {
       expect(SampleModule.purpose).toBe("AMD testing");
     });
+    
+    it('should store the jquery version', function() {
+      expect(SampleModule.jq_version).toBe("1.6");
+    });
   });
   
 	return {
