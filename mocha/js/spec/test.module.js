f901d1980abfad93a884298370d61a75ccd0f8be
Dave Geddes <davidcgeddes@gmail.com>
2011-12-16 17:00:48 -0700

    AMD tests working in the browser with mocha/chai
    to see it open mocha/runner.html in the browser

--- mocha/js/spec/test.module.js
+++ mocha/js/spec/test.module.js
@@ -2,15 +2,16 @@ define(["../SampleModule"], function(SampleModule) {
 
   describe("Sample Module", function() {
     it('should have a name', function() {
-      expect(SampleModule.name).toBe("sample");
+      expect(SampleModule.name).to.be.a("string");
+      expect(SampleModule.name).to.equal("sample");
     });
     
     it('should state the purpose', function() {
-      expect(SampleModule.purpose).toBe("AMD testing");
+      expect(SampleModule.purpose).to.equal("AMD testing");
     });
 
     it('should have its own dependencies', function() {
-      expect(SampleModule.dependency).toBe("Module B");
+      expect(SampleModule.dependency).to.equal("Module B");
     });
   });
   
