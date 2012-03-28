b5b6cc2fb16420751f2803aa7953dd0e4698b938
Dave Geddes <davidcgeddes@gmail.com>
2012-03-27 21:40:47 -0600

    fixed mocha tests while at it

--- mocha/js/spec/test.module.js
+++ mocha/js/spec/test.module.js
@@ -1,18 +1,26 @@
-define(["../SampleModule"], function(SampleModule) {
+define(["SampleModule"], function(SampleModule) {
+// define(function() {
 
   describe("Sample Module", function() {
-    it('should have a name', function() {
-      expect(SampleModule.name).to.be.a("string");
-      expect(SampleModule.name).to.equal("sample");
-    });
-    
-    it('should state the purpose', function() {
-      expect(SampleModule.purpose).to.equal("AMD testing");
-    });
+    // require(["../SampleModule"], function(SampleModule){
+      it('should have a name', function() {
+        expect(SampleModule.name).to.be.a("string");
+        expect(SampleModule.name).to.equal("sample");
+      });
+      
+      it('should state the purpose', function() {
+        expect(SampleModule.purpose).to.equal("AMD testing");
+      });
 
-    it('should have its own dependencies', function() {
-      expect(SampleModule.dependency).to.equal("Module B");
-    });
+      it('should have its own dependencies', function() {
+        expect(SampleModule.dependency).to.equal("Module B");
+      });
+
+      it('should have its own dependencies', function() {
+        expect(SampleModule.dependency).to.equal("Module B");
+      });
+    // });
+    
   });
   
 	return {
